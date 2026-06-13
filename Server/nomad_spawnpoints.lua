badfd026ddf5824927d667920b3eb0407255818f
MPF <pfmaxins@gmail.com>
2026-06-13 16:45:28 -0300

    Changes in spawpoint, personal config and other problems

---
+++ Server/nomad_spawnpoints.lua
@@ -0,0 +1,53 @@
+function SpawnPoints()
+	return {
+		unemployed = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		policeofficer = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		firefighter = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		doctor = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		nurse = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		construction_worker = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		engineer = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		farmer = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		burglar = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		chef = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		park_ranger = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		carpentry_prof = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		repairman = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		security_guard = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+		veteran = {
+			{ worldX = 4, worldY = 48, posX = 195, posY = 246 },
+		},
+	}
+end
+
+function SpawnRegions()
+	return {}
+end
---
+++ Server/nomad_spawnregions.lua
@@ -0,0 +1,3 @@
+function SpawnRegions()
+	return {}
+end
